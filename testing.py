88b138e5fb18b0f3b0d52390c00c89fb6af6ac71
Siddharth Prashant Kanikdale <106460051+Siddharth-0381@users.noreply.github.com>
2022-12-04 00:27:14 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,7 +8,8 @@ import streamlit as st
 model = tf.keras.models.load_model("hGDetection.h5")
 st.title("Hand Gesture Detection")
 st.sidebar.markdown(
-        """ Developed by Siddharth Kanikdale, Vedant Dawange, Anushka Yadav, Purva Potdukhe    
+        """ Developed by 
+        Siddharth Kanikdale, Vedant Dawange, Anushka Yadav, Purva Potdukhe    
             Email : spkanikdale@mitaoe.ac.in , vadawange@mitaoe.ac.in, aayadav@mitaoe.ac.in, purva.potdukhe@mitaoe.ac.in""")
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
